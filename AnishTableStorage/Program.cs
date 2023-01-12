975114dada19fb1033c523ca1e9cf2cc4ccd9e65
Anish Zala <azala@affirma.com>
2023-01-12 16:59:17 +0530

    Add project files.

---
+++ AnishTableStorage/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Azure.Data.Tables;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=anishstorage786;AccountKey=E0ShJ37KOKnzUF5p5ijvLKigF1Tm02OJPsKQ0UqA+ouiwPZ+LC0oMXt56Ky4Vg0dpaJdwiIA8UT++ASt4lT15Q==;EndpointSuffix=core.windows.net";
+string tableName = "Orders";
+
+AddEntity("O1", "Mobile", 100);
+AddEntity("O2", "Laptop", 50);
+AddEntity("O3", "Desktop", 70);
+AddEntity("O4", "Laptop", 200);
+
+void AddEntity(string orderID, string category, int quantity)
+{
+    TableClient tableClient = new TableClient(connectionString, tableName);
+
+    TableEntity tableEntity = new TableEntity(category, orderID)
+    {
+        {"quantity",quantity}
+    };
+
+    tableClient.AddEntity(tableEntity);
+    Console.WriteLine("Added Entity with order ID {0}", orderID);
+}
